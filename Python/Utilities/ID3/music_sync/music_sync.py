e4e447e944561935b56cc53dfd49be668b5d8402
Jerry Liu <jerrylarryliu@gmail.com>
2021-02-11 12:59:14 -0800

    update yt music uploader

--- Python/Utilities/ID3/music_sync/music_sync.py
+++ Python/Utilities/ID3/music_sync/music_sync.py
@@ -15,15 +15,20 @@ if __name__ == "__main__":
         for dirpath, dirnames, filenames in os.walk(local_folder):
             for filename in filenames:
                 if "mp3" in filename:
-                    audiofile = eyed3.load(dirpath + os.sep + filename)
-                    if audiofile.tag.album != "":
-                        local_list.append((audiofile.tag.album, audiofile.tag.album_artist, dirpath))
-                        local_list = list(set(local_list))
-                        break
+                    try:
+                        audiofile = eyed3.load(dirpath + os.sep + filename)
+                        if audiofile.tag.album != "":
+                            local_list.append((audiofile.tag.album, audiofile.tag.album_artist, dirpath))
+                            local_list = list(set(local_list))
+                            print(audiofile.tag.album_artist + ",", audiofile.tag.album + ",", dirpath)
+                            break
+                    except Exception as E:
+                        None
+        print()
     except Exception as E:
         print(E)
 
-    album_list, album_artist_list, path_list= [e[0] for e in local_list], [e[1] for e in local_list], [e[2] for e in local_list]
+    album_list, album_artist_list, path_list = [e[0] for e in local_list], [e[1] for e in local_list], [e[2] for e in local_list]
 
     f = open('headers_auth_raw.txt', 'r+')
     f_raw = f.read()
@@ -40,8 +45,21 @@ if __name__ == "__main__":
 
     diff = list(set(album_list).difference(set(ytmusic_list)))
 
+    print("LOCAL ALBUMS: ")
     upload_path_list = []
     for album, artist, path in local_list:
         for item in diff:
             if item == album:
+                index = album_list.index(album)
+                location = local_list[index][2]
+                upload_path_list.append(location)
                 print(artist, "-", album)
+    print()
+
+    for folder_path in upload_path_list:
+        for dirpath, dirnames, filenames in os.walk(folder_path):
+            for filename in filenames:
+                if "mp3" in filename:
+                    print("UPLOADING: " + dirpath + os.sep + filename)
+                    code = ytmusic.upload_song(dirpath + os.sep + filename)
+                    print(code)
