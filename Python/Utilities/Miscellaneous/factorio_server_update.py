d047dd17c2fc2e0586a5eaa936133181b0792cc3
Jerry Liu <jerrylarryliu@gmail.com>
2021-02-15 19:33:40 -0800

    update

--- Python/Utilities/Miscellaneous/factorio_server_update.py
+++ Python/Utilities/Miscellaneous/factorio_server_update.py
@@ -6,51 +6,63 @@ import paramiko
 import argparse
 
 
-def update_save(ssh, scp):
-    print("Updating Server Save File...")
+# TODO: add download save, add sync save (timestamp based)
+
+
+def upload_save(ssh, scp, local_path, server_path):
+    print("Uploading Server Save File...")
+
     print("Stopping factorio.service...")
     ssh.exec_command('systemctl stop factorio.service')
-
     time.sleep(3)
 
-    print("Updating save zip file via SCP...")
-    scp.put(local_save_path, server_save_path)
+    print("Uploading save zip file via SCP...")
+    scp.put(local_path, server_path)
 
     print("Restarting factorio.service...")
     ssh.exec_command('systemctl restart factorio.service')
-
     time.sleep(3)
 
     stdin, stdout, stderr = ssh.exec_command('systemctl status factorio.service --no-pager')
     print("".join(stdout.readlines()))
 
     stdin, stdout, stderr = ssh.exec_command('cd /opt/factorio/saves/; ls -al')
-    print("".join(stdout.readlines()))
+    print("".join(stdout.readlines()) + "\nUploading Complete!")
 
-    print("Update Complete!")
+
+def download_save(ssh, scp, local_path, server_path):
+    print("Downloading Server Save File...")
+
+    print("Stopping factorio.service...")
+    ssh.exec_command('systemctl stop factorio.service')
+    time.sleep(3)
+
+    print("Downloading save zip file via SCP...")
+    scp.get(server_path, local_path)
+    time.sleep(3)
+
+    stdin, stdout, stderr = ssh.exec_command('systemctl status factorio.service --no-pager')
+    print("".join(stdout.readlines()) + "\nDownload Complete!")
 
 
 def restart(ssh):
     print("Restarting factorio.service...")
     ssh.exec_command('systemctl restart factorio.service')
-
     time.sleep(5)
 
     stdin, stdout, stderr = ssh.exec_command('systemctl status factorio.service --no-pager')
-    print("".join(stdout.readlines()))
-
-    print("Restart Complete!")
+    print("".join(stdout.readlines()) + "\nRestart Complete!")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='', formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-u", "--update", action='store_true', help='Update Server Save File')
+    parser.add_argument("-u", "--upload", action='store_true', help='Upload Server Save File')
+    parser.add_argument("-d", "--download", action='store_true', help='Download Server Save File')
     parser.add_argument("-s", "--start", action='store_true', help='Start Server Factorio.service')
-
     inputs = parser.parse_args()
 
-    local_save_path = r"F:\Game\Factorio\saves\wonderland.zip"
     # local_save_path = os.getcwd() + os.sep + "saves" + os.sep + "wonderland.zip"
+    local_save_path = r"F:\Game\Factorio\saves\wonderland.zip"
     server_save_path = r"/opt/factorio/saves/wonderland.zip"
     server_ip = "192.168.1.120"
 
@@ -59,16 +71,23 @@ if __name__ == "__main__":
     ssh.connect(hostname=server_ip, username='root', password='1234', port=22)
     scp = scp.SCPClient(ssh.get_transport())
 
-    if inputs.update:
-        update_save(ssh, scp)
+    if inputs.upload:
+        upload_save(ssh, scp, local_save_path, server_save_path)
+    elif inputs.download:
+        download_save(ssh, scp, local_save_path, server_save_path)
     elif inputs.start:
         restart(ssh)
-    elif not inputs.update and not inputs.start:
-        input_value = input("Update(u) or Restart (s): ")
-
-        if input_value.lower() == "s":
+    elif not inputs.upload and not inputs.download and not inputs.start:
+        msg = "Select Function Index:\n[1] Upload Save\n[2] Download Save\n[3] Start Server\n"
+        input_value = input(msg)
+        if input_value == "1":
+            upload_save(ssh, scp, local_save_path, server_save_path)
+        elif input_value == "2":
+            download_save(ssh, scp, local_save_path, server_save_path)
+        elif input_value == "3":
             restart(ssh)
-        elif input_value.lower() == "u":
-            update_save(ssh, scp)
+        else:
+            sys.exit("ERROR: Invalid Input")
 
+    scp.close()
     time.sleep(3)
