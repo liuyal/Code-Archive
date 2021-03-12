413d56c952526ab24f4a0f4589e457a2d17a0a19
Jerry Liu <jerrylarryliu@gmail.com>
2021-03-11 18:03:27 -0800

    update

--- Python/Utilities/Miscellaneous/factorio_server_update.py
+++ Python/Utilities/Miscellaneous/factorio_server_update.py
@@ -52,6 +52,28 @@ def download_save(ssh, scp, pwd, local_path, server_path):
     print("".join(stdout.readlines()) + "\nDownload Complete!")
 
 
+def sync_mods(ssh, scp, pwd, local_mod_path, server_mod_path):
+    print("Stopping factorio.service...")
+    cmd = "echo " + pwd + " | sudo -S systemctl stop factorio.service"
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    time.sleep(3)
+
+    print("Uploading mod files via SCP...")
+    for item in os.listdir(local_mod_path):
+        scp.put(local_mod_path + os.sep + item, server_mod_path + '/' + item)
+        time.sleep(3)
+
+    print("Restarting factorio.service...")
+    cmd = "echo " + pwd + " | sudo -S systemctl restart factorio.service"
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    time.sleep(3)
+
+    cmd = "echo " + pwd + " | sudo -S systemctl status factorio.service --no-pager"
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    print("".join(stdout.readlines()) + "\nRestart Complete!")
+
+
+
 def restart(ssh, pwd):
     print("Stopping factorio.service...")
     cmd = "echo " + pwd + " | sudo -S systemctl stop factorio.service"
@@ -72,11 +94,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="", formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("-u", "--upload", action="store_true", help="Upload Server Save File")
     parser.add_argument("-d", "--download", action="store_true", help="Download Server Save File")
+    parser.add_argument("-m", "--mods", action="store_true", help="Upload Mod Files")
     parser.add_argument("-s", "--start", action="store_true", help="Start Server Factorio.service")
     inputs = parser.parse_args()
 
     local_save_path = r"F:\Game\Factorio\saves\wonderland.zip"
     server_save_path = r"/opt/factorio/saves/wonderland.zip"
+
+    local_mod_path = r"F:\Game\Factorio\mods\\"
+    server_mod_path = r"/opt/factorio/mods/"
 
     host_ip = "192.168.1.120"
     user = "wolf"
@@ -91,16 +117,20 @@ if __name__ == "__main__":
         upload_save(ssh, scp, password, local_save_path, server_save_path)
     elif inputs.download:
         download_save(ssh, scp, password, local_save_path, server_save_path)
+    elif inputs.mods:
+        sync_mods(ssh, scp, password, local_mod_path, server_mod_path)
     elif inputs.start:
         restart(ssh, password)
     elif not inputs.upload and not inputs.download and not inputs.start:
-        msg = "Select Function Index:\n[1] Upload Save\n[2] Download Save\n[3] Start Server\n"
+        msg = "Select Function Index:\n[1] Upload Save\n[2] Download Save\n[3] Sync Mods\n[4] Start Server\n"
         input_value = input(msg)
         if input_value == "1":
             upload_save(ssh, scp, password, local_save_path, server_save_path)
         elif input_value == "2":
             download_save(ssh, scp, password, local_save_path, server_save_path)
         elif input_value == "3":
+            sync_mods(ssh, scp, password, local_mod_path, server_mod_path)
+        elif input_value == "4":
             restart(ssh, password)
         else:
             sys.exit("ERROR: Invalid Input")
